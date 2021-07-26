a5e27ec16c2f7afc8251a44c48b42eabb17ca788
Gianluca <gl.galassini@gmail.com>
2021-07-26 16:15:57 +0200

    aggiustamenti finali snack 1

--- snack-1/nba.php
+++ snack-1/nba.php
@@ -1,6 +1,3 @@
-<!-- Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema.
-Olimpia Milano - Cantù | 55-60 -->
-
 <?php
 
 $matches_day_1 = [
@@ -38,7 +35,7 @@ $matches_day_1 = [
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>NBA - Day 1</title>
 </head>
 <body>
     <ul>
@@ -46,16 +43,13 @@ $matches_day_1 = [
         <?php for ($i = 0; $i < count($matches_day_1); $i++) { ?>
         <li>
             <?php echo (
-            $matches_day_1[$i]["home_name"] .
-            " - " .
-            $matches_day_1[$i]["away_name"] .
-            " | " . 
-            $matches_day_1[$i]["home_score"] .
-            "-" .
+            $matches_day_1[$i]["home_name"] . " - " .
+            $matches_day_1[$i]["away_name"] . " | " . 
+            $matches_day_1[$i]["home_score"] . "-" .
             $matches_day_1[$i]["away_score"]
-            ) ?>
+            );?>
         </li>
-        <?php } ?>
+        <?php }; ?>
     
     </ul>
 </body>
